24a988840431c0ff0bd2726db7a404c3bbc01165
Ravi Inder Manshahia <ravinder@lab91.co>
2019-02-13 10:09:48 +0530

    Initial Commit

---
+++ XYZ : Accelerometer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  XYZ : Accelerometer
+//
+//  Created by Ravi Inder Manshahia on 13/02/19.
+//  Copyright © 2019 Ravi Inder Manshahia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
