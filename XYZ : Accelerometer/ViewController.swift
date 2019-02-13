c971566d345416133d70e00ee62c9e5205fe907b
Ravi Inder Manshahia <ravinder@lab91.co>
2019-02-13 11:02:59 +0530

    Accelerometer Done

--- XYZ : Accelerometer/ViewController.swift
+++ XYZ : Accelerometer/ViewController.swift
@@ -7,19 +7,44 @@
 //
 
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
 
+    var motionManager : CMMotionManager!
+    
+    @IBOutlet weak var xLabel: UILabel!
+    @IBOutlet weak var yLabel: UILabel!
+    @IBOutlet weak var zLabel: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        motionManager = CMMotionManager()
+        motionManager.startAccelerometerUpdates(to: .main, withHandler: updateLabels)
+
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func updateLabels( data : CMAccelerometerData?, error : Error?)
+    {
+        if let accData = data
+        {
+            let formatter = NumberFormatter()
+            formatter.minimumFractionDigits = 1
+            formatter.maximumFractionDigits = 1
+            
+            let x = formatter.string(for: accData.acceleration.x)!
+            let y = formatter.string(for: accData.acceleration.y)!
+            let z = formatter.string(for: accData.acceleration.z)!
+            
+            xLabel.text = "X : \(x)"
+            yLabel.text = "Y : \(y)"
+            zLabel.text = "Z : \(z)"
+        }
+        else
+        {
+            return
+        }
     }
-
-
 }
 
